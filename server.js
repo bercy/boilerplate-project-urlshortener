856a1aca42944cf6c20e10868502dc6860daaad6
Glitch (bercy-boilerplate-project-urlshortener) <none>
2019-02-22 15:16:13 +0000

    🍂🏹 Checkpoint ./app/routes/redirectToUrl.js:3154011/149 ./server.js:3154011/1521

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var AddNewUrl = require('./app/routes/addNewUrl');
-var RedirectToUrl = require('./app/routes/redirectToUrl');
-var Url = require('./app/models/url');
 var validator = require('validator');
 
 var cors = require('cors');
@@ -16,35 +13,41 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 
 app.use(cors());
-
-/** this project needs to parse POST bodies **/
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// static files
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+let Url = mongoose.model('Url', new mongoose.Schema({
+  url : {type: String, required: true}
+}));
+
+app.post('/api/shorturl/new', (req, res) => {
+  if (!validator.isURL(req.body.url)) {
+      return res.json({error: 'invalid URL'});
+  }
+    
+  Url.create({url: req.body.url}, (err, data) => {
+    if (err) {
+      res.json({error: "can't write db"});
+    }
+    
+    res.json({original_url: data.url, short_url: data.id});
+  });
 });
 
-let handler;
-const urlModel = new Url(mongoose);
-// create url
-handler = new AddNewUrl(urlModel, validator);
-app.post('/api/shorturl/new', handler.invoke.bind(handler));
 
-// redirect to url
-handler = new RedirectToUrl(urlModel);
-app.get('/api/shorturl/:id', handler.invoke.bind(handler));
+app.get('/api/shorturl/:id', (req, res) => {
+  
+});
 
 
 app.listen(port, function () {
