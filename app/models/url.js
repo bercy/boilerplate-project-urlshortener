3b4ee331dc8b5ef2dae8228f1097c5c38b7eba40
Glitch (bercy-boilerplate-project-urlshortener) <none>
2019-02-22 12:17:17 +0000

    🎳👊 Checkpoint ./app/models/url.js:3154011/583

--- app/models/url.js
+++ app/models/url.js
@@ -1,20 +1,22 @@
 module.exports = class Url {
   constructor (Schema) {
-    this._Schema = Schema;    
+    this._Schema = Schema;   
+    
+    var personSchema = new Schema({
+      name: {
+        type: String,
+        required: [true, 'Why no name?']
+      },
+      age: Number,
+      favoriteFoods: [String]
+    });
+
+
+  var Person = mongoose.model('Person', personSchema);
   }
   
 
 
-var personSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Why no name?']
-  },
-  age: Number,
-  favoriteFoods: [String]
-});
 
-
-var Person = mongoose.model('Person', personSchema);
 
 }
